4c2c2f03218d851feec1dbe435181d0e467566c7
Martins-O-U <martinsonyedikachi@gmail.com>
2019-11-14 17:28:28 +0100

    Created a server with a dummy function with a successful test

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-require('dotenv').config();
-
 const server = require('./server');
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 server.listen(port, () => console.log(`Listening on port ${port}`));
